11e64e024bf44028e65a473a4da3df717092b53f
doudousha <dou_dou_sha@sina.com>
2017-09-11 14:19:15 +0800

    新增spring boot 模板thymeleaf

---
+++ springthymeleafsimple/src/test/java/com/wangqing/springthymeleafsimple/SpringThymeleafSimpleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wangqing.springthymeleafsimple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringThymeleafSimpleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
